f66e3f53186db09bfdc42598a48095ed64fba6a8
crawkatt <man849916@gmail.com>
2023-10-04 12:15:51 -0300

    🎨 format: formateando código

--- src/commands/fun/mod.rs
+++ src/commands/fun/mod.rs
@@ -22,13 +22,8 @@ use teloxide_core::{
 };
 
 pub async fn send(bot: Bot, msg: Message) -> ResponseResult<()> {
-    let Some(text) = msg.text() else {
-        return Ok(())
-    };
-
-    let Some(user) = msg.from() else {
-        return Ok(())
-    };
+    let Some(text) = msg.text() else { return Ok(()) };
+    let Some(user) = msg.from() else { return Ok(()) };
 
     let user_id = user.id;
     let ChatId(user_id) = ChatId::from(user_id);
@@ -43,9 +38,7 @@ pub async fn send(bot: Bot, msg: Message) -> ResponseResult<()> {
 
     let action = parts[1];
     let category_result = nekosbest::Category::from_str(action);
-    let Ok(category) = category_result else {
-        return Ok(())
-    };
+    let Ok(category) = category_result else { return Ok(()) };
 
     let random_gif = nekosbest::get(category).await;
     let username_target = parts[2];
@@ -55,29 +48,20 @@ pub async fn send(bot: Bot, msg: Message) -> ResponseResult<()> {
 
     let message = match action {
         "kiss" => format!("{username_author} besó a {username_target}"),
-        "cuddle" => {
-            format!("{username_author} envuelve a {username_target} con un fuerte abrazo")
-        }
+        "cuddle" => format!("{username_author} envuelve a {username_target} con un fuerte abrazo"),
         "hug" => format!("{username_author} abrazó a {username_target}"),
         "pat" => format!("{username_author} acarició a {username_target}"),
         "slap" => format!("{username_author} cacheteó a {username_target}"),
         "kick" => format!("{username_author} pateó a {username_target}"),
         "punch" => format!("{username_author} le dio un puñetazo a {username_target}"),
         "shoot" => format!("{username_author} le disparó a {username_target}"),
-        "yeet" => {
-            format!("{username_author} mandó a {username_target} a la punta del cerro")
-        }
+        "yeet" => format!("{username_author} mandó a {username_target} a la punta del cerro"),
         _ => format!("{action} no es una categoria valida"),
     };
 
     let random_gif = random_gif.map_err(|_| ());
-
-    let Ok(random_gif) = random_gif else {
-        return Ok(())
-    };
-
+    let Ok(random_gif) = random_gif else { return Ok(()) };
     let gif_url = random_gif.url.parse();
-
     let Ok(url) = gif_url else { return Ok(()) };
 
     let ok = bot
