6a364f3f3bedf706210764001f96213a9b995ff0
crawkatt <man849916@gmail.com>
2023-10-04 21:53:03 -0300

    ✨ feat: Añadiendo filtro anti-rusos

--- src/handlers/chat_member_updates.rs
+++ src/handlers/chat_member_updates.rs
@@ -9,25 +9,40 @@ use teloxide_core::{
     types::ChatMemberUpdated,
 };
 
+const BANNED_CHARS: [&str; 42] = [
+    "Б", "Г", "Д", "Ё", "Ж", "З",
+    "И", "Й", "Л", "П", "Ф", "Ц",
+    "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь",
+    "Э", "Ю", "Я", "б", "г", "д",
+    "ё", "ж", "з", "и", "й", "л",
+    "п", "ф", "ц", "ч", "ш", "щ",
+    "ъ", "ы", "ь", "э", "ю", "я",
+];
+
 pub async fn new_chat_member(
     bot: Bot,
     chat_member: ChatMemberUpdated,
 ) -> ResponseResult<()> {
-    let user = chat_member.old_chat_member.user.clone();
-
+    let user = chat_member.old_chat_member.user;
     let user_id = user.id;
-
+    let first_name = &user.first_name;
     let telegram_group_name = chat_member.chat.title().unwrap_or("");
-
     let ChatId(user_id) = ChatId::from(user_id);
-
-    let username = user
-        .mention()
+    let username = user.mention()
         .unwrap_or_else(|| html::user_mention(user_id, user.full_name().as_str()));
+
+    if first_name.len() >= 3 && BANNED_CHARS.iter().any(|&banned_char| first_name.contains(banned_char)) {
+        bot.ban_chat_member(chat_member.chat.id, user.id).await?;
+        bot.send_message(
+            chat_member.chat.id,
+            format!("{username} Contiene caracteres Rusos, \nAccion: baneado!"),
+        ).await?;
+        return Ok(())
+    }
 
     bot.send_message(
         chat_member.chat.id,
-        format!("Welcome to {telegram_group_name} {username}!"),
+        format!("Bienvenido a {telegram_group_name} {username}!"),
     )
     .await?;
 
@@ -39,16 +54,13 @@ pub async fn left_chat_member(
     chat_member: ChatMemberUpdated,
 ) -> ResponseResult<()> {
     let user = chat_member.old_chat_member.user;
-
     let user_id = user.id;
-
     let ChatId(user_id) = ChatId::from(user_id);
-
     let username = user
         .mention()
         .unwrap_or_else(|| html::user_mention(user_id, user.full_name().as_str()));
 
-    bot.send_message(chat_member.chat.id, format!("Goodbye {username}!"))
+    bot.send_message(chat_member.chat.id, format!("Hasta pronto {username}!"))
         .await?;
 
     Ok(())
