06998b7e935dac058211537fee4bc924e4a98978
crawkatt <man849916@gmail.com>
2023-10-05 21:44:09 -0300

    ✨ feat: Nuevos métodos para resolver diferentes variantes del comando unban ♻️ Refactor: mejorando estructura del código

--- src/commands/admin/unban.rs
+++ src/commands/admin/unban.rs
@@ -2,58 +2,51 @@ use teloxide_core::payloads::SendMessageSetters;
 use teloxide_core::prelude::{Requester, UserId};
 use teloxide_core::requests::ResponseResult;
 use teloxide_core::types::Message;
-use crate::error::{USER_NOT_FOUND, NOT_BANNED};
+use crate::error::{PermissionsDenied, handle_status};
 use crate::prelude::Bot;
-use crate::utils::{AdminOrOwner, MessageExt, Timer};
+use crate::utils::{MessageExt, Timer};
 
 pub async fn unbanning(bot: Bot, msg: Message) -> ResponseResult<()> {
     let chat_id = msg.chat.id;
+    let user_status = handle_status(&bot, &msg).await;
+    if !user_status {
+        bot.send_message(msg.chat.id, PermissionsDenied)
+            .reply_to_message_id(msg.id).await?
+            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
+        return Ok(())
+    }
+
+    // Necessary for ban by id (/ban 12345678)
     let Some(replied) = msg.reply_to_message() else {
         bot.unban_chat_member(chat_id, UserId(msg.parse_id())).await?;
         bot.send_message(msg.chat.id, "✅ Usuario desbaneado")
             .reply_to_message_id(msg.id).await?
-            .delete_message_timer(bot, msg.chat.id, msg.id, 60);
+            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
         return Ok(())
     };
 
+    // Unban for reply to a Join Event (/ban <user> Joined the group)
     let Some(user) = replied.from() else {
-        bot.send_message(msg.chat.id, USER_NOT_FOUND)
-            .reply_to_message_id(msg.id)
-            .await?
-            .delete_message_timer(bot, msg.chat.id, msg.id, 60);
+        let Some(user) = msg.extract_first_new_member(&msg) else {
+            return Ok(())
+        };
+
+        bot.unban_chat_member(chat_id, user.id).await?;
+        bot.send_message(msg.chat.id, "✅ Usuario desbaneado")
+            .reply_to_message_id(msg.id).await?
+            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
 
         return Ok(())
     };
 
-    let Some(from) = msg.from() else {
-        return Ok(())
-    };
-
+    // Ban for reply to a message (/ban <replying message>)
     let user_id = user.id;
-
     let username = user.username
         .as_ref()
         .map_or_else(String::new, std::string::ToString::to_string);
 
-    let is_admin_or_owner = bot.get_chat_member(msg.chat.id, from.id)
-        .await?
-        .is_admin_or_owner();
-
-    let chat_member = bot.get_chat_member(msg.chat.id, user_id)
-        .await?
-        .status()
-        .is_banned();
-
-    if is_admin_or_owner && !chat_member {
-        bot.send_message(msg.chat.id, format!("❌ @{username} [<code>{user_id}</code>] {NOT_BANNED}"))
-            .await?
-            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
-
-        return Ok(())
-    }
-
     bot.unban_chat_member(msg.chat.id, user_id).await?;
-    bot.send_message(msg.chat.id, format!("✅ @{username} [<code>{user_id}</code>] ha sido baneado"))
+    bot.send_message(msg.chat.id, format!("✅ @{username} [<code>{user_id}</code>] ha sido desbaneado"))
         .reply_to_message_id(msg.id).await?.delete_message_timer(bot, msg.chat.id, msg.id, 60);
 
     Ok(())
