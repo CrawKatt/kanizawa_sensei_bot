f4f136e87204c1ca5650c04602aa9f34ca9120b8
crawkatt <man849916@gmail.com>
2023-10-09 13:54:38 -0300

    ✨ feat: Nuevos comandos mute y unmute

---
+++ src/commands/admin/unmute.rs
@@ -0,0 +1,59 @@
+use teloxide_core::{
+    payloads::SendMessageSetters,
+    requests::ResponseResult,
+    prelude::{
+        Requester,
+        UserId
+    },
+    types::{
+        Message,
+        ChatPermissions
+    }
+};
+use crate::prelude::Bot;
+use crate::error::{PermissionsDenied, handle_status};
+use crate::utils::{MessageExt, Timer};
+
+pub async fn unmuting(bot: Bot, msg: Message) -> ResponseResult<()> {
+    let user_status = handle_status(&bot, &msg).await;
+    if !user_status {
+        bot.send_message(msg.chat.id, PermissionsDenied)
+            .reply_to_message_id(msg.id).await?
+            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
+        return Ok(())
+    }
+
+    // Necesario para el Unmute por id (/unmute 12345678)
+    let Some(replied) = msg.reply_to_message() else {
+        bot.restrict_chat_member(msg.chat.id, UserId(msg.parse_id()),ChatPermissions::all()).await?;
+        bot.send_message(msg.chat.id, "✅ Usuario silenciado")
+            .reply_to_message_id(msg.id).await?
+            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
+        return Ok(())
+    };
+
+    // Unmute for reply to a Join Event (/unmute <user> Joined the group)
+    let Some(user) = replied.from() else {
+        let Some(user) = msg.extract_first_new_member(&msg) else {
+            return Ok(())
+        };
+        bot.restrict_chat_member(msg.chat.id, user.id,ChatPermissions::all()).await?;
+        bot.send_message(msg.chat.id, "✅ Se ha removido el silencio al usuario")
+            .reply_to_message_id(msg.id).await?
+            .delete_message_timer(bot, msg.chat.id, msg.id, 10);
+
+        return Ok(())
+    };
+
+    // Unmute for reply to a message (/unmute <replying message>)
+    let user_id = user.id;
+    let username = user.username
+        .as_ref()
+        .map_or_else(String::new, std::string::ToString::to_string);
+
+    bot.restrict_chat_member(msg.chat.id, user_id,ChatPermissions::all()).await?;
+    bot.send_message(msg.chat.id, format!("✅ @{username} [<code>{user_id}</code>] ha sido silenciado"))
+        .reply_to_message_id(msg.id).await?.delete_message_timer(bot, msg.chat.id, msg.id, 60);
+
+    Ok(())
+}
